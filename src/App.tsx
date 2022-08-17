3ecbd53a88cfc4709d61930e98cd937670024ddd
Davi Lima <davirlima@pop-os.localdomain>
2022-08-17 11:17:19 -0400

    Componente Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./global.css";
 import { Header } from "./components/Header";
 import { FormTask } from "./components/FormTask";
+import { Tasks } from "./components/Tasks";
 
 export function App() {
   return (
     <div>
       <Header />
       <FormTask />
+      <Tasks />
     </div>
   );
 }
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,26 @@
+import { ClipboardText } from "phosphor-react";
+import styles from "./Tasks.module.css";
+
+export function Tasks() {
+  return (
+    <div className={styles.tasksArea}>
+      <div className={styles.informationTasks}>
+        <h2 className={styles.createdTasks}>
+          Tarefas criadas <span>0</span>
+        </h2>
+        <h2 className={styles.concludedTasks}>
+          Concluídas <span>0</span>
+        </h2>
+      </div>
+      <div className={styles.defaultInformation}>
+        <ClipboardText size={56} color="var(--gray-400)" />
+        <h2 className={styles.titleDefaultInformation}>
+          Você ainda não tem tarefas cadastradas
+        </h2>
+        <h3 className={styles.subtitleDefaultInformation}>
+          Crie tarefas e organize seus itens a fazer
+        </h3>
+      </div>
+    </div>
+  );
+}
