656a439540bb673940fd72d40609ae08226846ef
Davi Lima <davirlima@pop-os.localdomain>
2022-08-16 20:21:00 -0400

    Componentes Header e FormTask

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./global.css";
+import { Header } from "./components/Header";
+import { FormTask } from "./components/FormTask";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <FormTask />
+    </div>
+  );
+}
