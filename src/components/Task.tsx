4d5cffc7c2a2f131146c06bb23ea2ea58bb7d52d
Davi Lima <davi.rlima04@gmail.com>
2022-08-18 21:04:42 -0400

    Função de Marcar como concluído

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,14 +2,23 @@ import { Check, ClipboardText, Trash } from "phosphor-react";
 import styles from "./Task.module.css";
 
 interface TaskProps {
-  newTask: string;
+  task: {
+    id: string;
+    content: string;
+    isCompleted: boolean;
+  };
   isDefaultReturn: boolean;
-  onDeleteTask: (taskToDelete: string) => void;
+  deleteThisTask: (idOfTaskToDelete: string) => void;
+  changeStatusOfThisTask: (idOfTaskToChange: string) => void;
 }
 
 export function Task(props: TaskProps) {
   function deleteThisTask() {
-    props.onDeleteTask(props.newTask);
+    props.deleteThisTask(props.task.id);
+  }
+
+  function changeStatusOfThisTask() {
+    props.changeStatusOfThisTask(props.task.id);
   }
 
   if (props.isDefaultReturn == true) {
@@ -24,15 +33,15 @@ export function Task(props: TaskProps) {
         </h3>
       </div>
     );
-  } else if (props.newTask !== "") {
+  } else if (props.task.content !== "default") {
     return (
       <div className={styles.taskContainer}>
-        <input id="checkbox" type="checkbox" value={props.newTask} />
+        <input id="checkbox" type="checkbox" onClick={changeStatusOfThisTask} />
         <span>
           <Check className={styles.checkIcon} weight="bold" />
         </span>
         <label className={styles.taskContent} htmlFor="checkbox">
-          {props.newTask}
+          {props.task.content}
         </label>
         <button onClick={deleteThisTask} className={styles.deleteButton}>
           <Trash size={20} />
@@ -40,6 +49,6 @@ export function Task(props: TaskProps) {
       </div>
     );
   } else {
-    return <span></span>;
+    return <></>;
   }
 }
--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -2,25 +2,76 @@ import { ClipboardText, PlusCircle } from "phosphor-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Task } from "./Task";
 import styles from "./TaskArea.module.css";
+import { v4 as uuidv4 } from "uuid";
 
 export function TaskArea() {
+  const [tasks, setTasks] = useState([
+    {
+      id: "0",
+      content: "default",
+      isCompleted: false,
+    },
+  ]);
+
   const [taskText, setTaskText] = useState("");
+
   function handleAtualizeTaskText(event: ChangeEvent<HTMLInputElement>) {
     setTaskText(event.target.value);
   }
 
-  const [tasks, setTasks] = useState([""]);
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
-    setTasks([...tasks, taskText]);
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        content: taskText,
+        isCompleted: false,
+      },
+    ]);
     setTaskText("");
   }
 
-  function handleDeleteTask(taskToDelete: string) {
+  function handleDeleteTask(idOfTaskToDelete: string) {
     const tasksToKeep = tasks.filter((tasks) => {
-      return tasks !== taskToDelete;
+      return tasks.id !== idOfTaskToDelete;
     });
     setTasks(tasksToKeep);
+  }
+
+  function handleChangeStatusTask(idOfTaskToChengeStatus: string) {
+    const tasksWithModifiedyStatus = tasks.map((task) => {
+      if (task.id == idOfTaskToChengeStatus) {
+        switch (task.isCompleted) {
+          case true:
+            task.isCompleted = false;
+            break;
+          case false:
+            task.isCompleted = true;
+            break;
+        }
+      }
+      return task;
+    });
+    setTasks(tasksWithModifiedyStatus);
+  }
+
+  function calculateConcludedTasks() {
+    const numberOfConcludedTasks = tasks.reduce(function (
+      allCompletedTasks,
+      task
+    ) {
+      if (task.isCompleted == true) {
+        allCompletedTasks++;
+      }
+      return allCompletedTasks;
+    },
+    0);
+    if (numberOfConcludedTasks == 0) {
+      return numberOfConcludedTasks.toString();
+    } else {
+      return `${numberOfConcludedTasks.toString()} de ${tasks.length - 1}`;
+    }
   }
 
   return (
@@ -45,17 +96,19 @@ export function TaskArea() {
             Tarefas criadas <span>{tasks.length - 1}</span>
           </h2>
           <h2 className={styles.concludedTasks}>
-            Concluídas <span>0</span>
+            Concluídas
+            <span>{calculateConcludedTasks()}</span>
           </h2>
         </div>
         <form>
           {tasks.map((task) => {
             return (
               <Task
-                key={task}
-                newTask={task}
+                key={task.id}
+                task={task}
                 isDefaultReturn={tasks.length == 1}
-                onDeleteTask={handleDeleteTask}
+                deleteThisTask={handleDeleteTask}
+                changeStatusOfThisTask={handleChangeStatusTask}
               />
             );
           })}
