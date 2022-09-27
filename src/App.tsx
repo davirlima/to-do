8a99ca1b3a1fe9a6b057bb10baa518d9ecec30cf
Davi Lima <davi.rlima04@gmail.com>
2022-09-26 23:00:38 -0400

    Salvando tema no LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,24 @@ import { GlobalStyle } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { DarkTheme } from "./styles/themes/dark";
 import { Moon, Sun } from "phosphor-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LightTheme } from "./styles/themes/ligth";
 import Switch from "react-switch";
 import { Footer } from "./components/Footer";
 
 export function App() {
-  const [themeApplication, setThemeApplication] = useState(LightTheme);
+  const [themeApplication, setThemeApplication] = useState(() => {
+    const themeOnStorage = localStorage.getItem("theme");
+    if (themeOnStorage) {
+      return JSON.parse(themeOnStorage);
+    } else {
+      return LightTheme;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(themeApplication));
+  }, [themeApplication]);
 
   return (
     <ThemeProvider theme={themeApplication}>
@@ -25,7 +36,7 @@ export function App() {
               themeApplication == DarkTheme ? LightTheme : DarkTheme
             );
           }}
-          checked={themeApplication === DarkTheme}
+          checked={themeApplication.title === DarkTheme.title}
           checkedIcon={<Sun size={25} color="#1a1a1a" />}
           uncheckedIcon={<Moon size={25} color="#dadada" />}
           onHandleColor="#1a1a1a"
